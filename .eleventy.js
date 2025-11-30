d5c048c6dceb2b822e7826d0aca775699181d23b
JFab68 <42310697+JFab68@users.noreply.github.com>
2025-11-30 02:23:25 -0700

    Add full program pages and updates

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "src/css": "css" });
+  eleventyConfig.addPassthroughCopy({ "src/js": "js" });
+  eleventyConfig.addPassthroughCopy({ "src/images": "images" });
+  eleventyConfig.addPassthroughCopy({ "src/programs": "programs" });
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "dist"
+    },
+    templateFormats: ["njk", "html"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    passthroughFileCopy: true
+  };
+};
